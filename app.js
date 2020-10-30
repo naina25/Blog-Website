a9ef30c130566a97fbb1989882171447bc6b0d48
naina <nanu02d8@gmail.com>
2020-10-30 13:37:00 +0530

    Add styles and render home.ejs page

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get("/", (req,res) => {
+    res.render("home", {contentHome: homeStartingContent});
+})
 
 
 
