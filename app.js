828ff41ed4a305cd26fe5b9aa909d7833206321e
naina <nanu02d8@gmail.com>
2020-12-22 12:53:05 +0530

    connect database and edit get and post route

--- app.js
+++ app.js
@@ -3,11 +3,24 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
 const { request } = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const posts = [];
+
+const postSchema = {
+    title: {
+        type: String,
+        required: true
+    },
+    textarea: {
+        type: String,
+        required: true
+    }
+}
+const Post = mongoose.model("Post", postSchema);
 
 const app = express();
 
@@ -16,9 +29,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
-app.get("/", (req,res) => {
-    res.render("home", {contentHome: homeStartingContent, newPosts: posts});
+const MONGO_URI = process.env.MONGO_URI;
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+app.get("/", async(req,res) => {
+    try {
+        const posts = await Post.find()
+        res.render("home", {contentHome: homeStartingContent, newPosts: posts});
+    } catch (error) {
+        console.log(error);
+    }
+    
 });
 
 app.get("/about", (req,res) => {
@@ -34,23 +58,21 @@ app.get("/compose", (req,res) => {
 });
 
 app.post("/compose",(req,res) => {
-    let post = {
-        title: req.body.title,
-        textarea: req.body.textarea
-    }
-    posts.push(post);
+    title = req.body.title,
+    textarea = req.body.textarea
+    const post = new Post({title: title, textarea: textarea})
+    post.save();
     res.redirect("/");
 })
 
-app.get("/posts/:topic", function (req, res) {
-    const requestedTitle = _.lowerCase(req.params.topic);
-    posts.forEach((post)=>{
-        const storedTitle = _.lowerCase(post.title);
-        if(storedTitle === requestedTitle){
-            res.render("post", {postTitle: post.title, postBody: post.textarea})
-        }
-    })
-  })
+app.get("/posts/:postId", async(req, res)=> {
+    try {
+        const post = await Post.findById({_id: req.params.postId});
+        res.render("post", {postTitle: post.title, postBody: post.textarea})
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
